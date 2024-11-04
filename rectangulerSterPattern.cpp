0a1a50841707cc03876c37b3da7b22ee571d8719
zihadulislam2 <zihadul708@gmail.com>
2024-11-04 20:26:12 +0600

    ectanguler ster pattern

---
+++ rectangulerSterPattern.cpp
@@ -0,0 +1,31 @@
+/*
+state:
+- cpp basic structure.
+- include library 
+- create custom function using void
+-
+- outer for loop that loop from the row
+- inner for loop that loop and create the colum
+- ger paramater of int N
+- inter function and outer function work as 4^2
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern1 (int N) {
+    for ( int row = 0; row < N;  row++)
+    {
+        for ( int col = 0; col < N; col++)
+        {
+            cout<<"*";
+        }
+        cout<< endl;
+    }
+}
+
+int main () {
+    int N = 4;
+    pattern1(N);
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"hello world";
+    return 0;
+}
+
+// ctrl+shift+B to build.
+
+Rectangular Star Pattern
